24ddad41220d1b841cd78efdf7d4d8906cee705e
goosius1 <goosius1@gmail.com>
2021-08-25 13:48:36 +0100

    Added configs

--- src/main/java/io/github/townyadvanced/townyresources/settings/TownyResourcesConfigNodes.java
+++ src/main/java/io/github/townyadvanced/townyresources/settings/TownyResourcesConfigNodes.java
@@ -253,7 +253,86 @@ public enum TownyResourcesConfigNodes {
 			"# 1 - The name of the category (used for messaging)",
 			"# 2 - The discovery weight of the category (used during discovery)",
 			"# 3-  The base amount of the offer (in stacks)",
-			"# 4-  The list of materials in the category");
+			"# 4-  The list of materials in the category"),
+	REDUCE_ITEMS(
+			"reduce_items",
+			"",
+			"",
+			"",
+			"############################################################",
+			"# +------------------------------------------------------+ #",
+			"# |                    Reduce Items                      | #",
+			"# +------------------------------------------------------+ #",
+			"############################################################",
+			""),
+	REDUCE_ITEMS_ENABLED(
+			"reduce_items.enabled",
+			"false",
+			"",
+			"# If true, the reduce items command can be run.",
+			"# if false, the reduce items command cannot be run."),
+	REDUCE_ITEMS_START_DELAY_MINUTES(
+			"reduce_items.start_delay_minutes",
+			"5",
+			"",
+			"# The duration of the delay after the command is run, and BEFORE the reduction process starts."),
+	REDUCE_ITEMS_BATCH_SIZE_NUM_CHUNKS(
+			"reduce_items.batch_size_num_chunks",
+			"2500",
+			"",
+			"# The number of chunks to process in each batch."),
+	REDUCE_ITEMS_PAUSE_AFTER_EACH_BATCH_MILLIS(
+			"reduce_items.pause_after_each_batch_millis",
+			"2500",
+			"",
+			"# After each batch is processed, the code will paused temporarily. This is to help not overload the server etc."),
+	REDUCE_ITEMS_DISCONNECT_PLAYERS_WHEN_REDUCTION_PROCESS_IS_RUNNING(
+			"reduce_items.disconnect_players_when_reduction_process_is_running",
+			"true",
+			"",
+			"# If true, players are kicked when the reduction process starts, and cannot connect while it is running.",
+			"# Players with the permission to run the reduce command are immune, as are OP's.",
+			"# This setting is recommended, otherwise players would certainly move items around the map to avoid them getting reduced."),
+	REDUCE_ITEMS_MATERIALS(
+			"reduce_items.materials",
+			"{GRANITE, DIORITE, ANDESITE, GOLD_INGOT, 30%}, {OAK_LOG, OAK_LOG, 10%}, {EMERALD, EMERALD, 100%}",
+			"",
+			"# The materials which will be affected by the reduction process.",
+			"# There can be any number of entries (the default example is just 3).",
+			"# Each entry is in the format:  {<SOURCE_MATERIAL_1>, <SOURCE_MATERIAL_2> .... <FINAL_MATERIAL>, <PERCENTAGE_STACK_REDUCTION>}.",
+			"# ",
+			"# The algorithm works like this:",
+			"# 1. All player inventories and enderchests are scanned",
+			"# 2. All containers in the affected areas are scanned:",
+			"# 3. IMPORTANT NOTE: BLOCKS ARE IGNORED AND NOT SCANNED!)",
+			"# 4. During scanning, whenever an item stack is found: If any entry lists that item stack as a SOURCE_MATERIAL, then the item stack is converted to the FINAL_MATERIAL of that entry, and its amount is reduced by the PERCENTAGE_STACK_REDUCTION of that entry."),
+	REDUCE_ITEMS_AFFECTED_AREAS(
+			"reduce_items.affected_areas",
+			"",
+			"",
+			""),
+	REDUCE_ITEMS_AFFECTED_AREAS_LOCATIONS(
+			"reduce_items.affected_areas.locations",
+			"{world: -1000, -1000, 1000, 1000} {nether, -100, -100, 100, 100}, {end -100, -100, 100, 100}",
+			"",
+			"# The world locations which will be affected by the reduction process.",
+			"# The format of each entry defines a rectangle: {<world name>, <topLeftX>, <topLeftY>, <bottomRightX>, <bottomRightY>."),			
+	REDUCE_ITEMS_AFFECTED_AREAS_START_LOCATION(
+			"reduce_items.affected_areas.start_location",
+			"{world: -1000, -1000}",
+			"",
+			"# Indicates where the reduction process should start.",
+			"# Useful if a reduction process gets interrupted."),			
+	REDUCE_ITEMS_AFFECTED_AREAS_PLAYER_INVENTORIES(
+			"reduce_items.affected_areas.player_inventories",
+			"true",
+			"",
+			"# If true, player inventories will be affected by the reduction process."),
+	REDUCE_ITEMS_AFFECTED_AREAS_ENDERCHEST_INVENTORIES(
+			"reduce_items.affected_areas.enderchest_inventories",
+			"true",
+			"",
+			"# If true, enderchest inventories will be affected by the reduction process.");
 
 	private final String Root;
 	private final String Default;
